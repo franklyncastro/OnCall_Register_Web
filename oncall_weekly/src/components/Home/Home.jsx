74188a661a0e1b87283411b9a537ffdd072b854c
Franklyn Y. Castro Leyba <fcastro@viva.com.do>
2025-11-21 16:11:57 -0400

    omitir

--- oncall_weekly/src/components/Home/Home.jsx
+++ oncall_weekly/src/components/Home/Home.jsx
@@ -1,18 +1,18 @@
 import '../../../src/style/Home.css'
+import RataLoading from '../RataLoading/RataLoading'
 import TextType from '../TypeText/Typetext'
 
 
 export default function Home() {
   return (
-    <div>
-       
-     <TextType 
-      text={["La disciplina ", "tarde o temprano", "supera al talento.!"]}
-      typingSpeed={75}
-      pauseDuration={1500}
-      showCursor={true}
-      cursorCharacter="|"
-    />
+    <div className='container_home'>
+        <TextType 
+          text={["La disciplina ", "tarde o temprano", "supera al talento.!"]}
+          typingSpeed={75}
+          pauseDuration={1500}
+          showCursor={true}
+          cursorCharacter="|"
+        />
 
     </div>
   )
